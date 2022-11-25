6646b565d26025d25dd36bdcbc2e0922cca9dcaa
Aneesh <katta.aneesh597@gmail.com>
2022-11-25 14:03:53 +0530

    added uc1

--- GenericAssginment-batch222/MaximumNumberCheck.cs
+++ GenericAssginment-batch222/MaximumNumberCheck.cs
@@ -8,10 +8,42 @@ namespace GenericAssignment_batch222
 {
     internal class MaximumNumberCheck
     {
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return +firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return +secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return +thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+
+        }
         public static void Main()
         {
             Console.WriteLine("Welcome to Generic's Maximum Number Check");
-           
+            Console.WriteLine("Enter three integer values find the max Number");
+            Console.WriteLine("Enter firstValue");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter secondValue");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter thirdValue");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int output = MaximumNumberCheck.MaximumIntegerNumber(x, y, z);
+            Console.WriteLine(output + " is Maximum number");
+            Console.ReadKey();
         }
 
     }
