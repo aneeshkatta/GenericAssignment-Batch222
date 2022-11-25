4751042db94861659b13ed7c1502c7025a557983
Aneesh <katta.aneesh597@gmail.com>
2022-11-25 15:44:04 +0530

    added refactor find max value generic uc

--- GenericAssginment-batch222/MaximumNumberCheck.cs
+++ GenericAssginment-batch222/MaximumNumberCheck.cs
@@ -6,45 +6,31 @@ using System.Threading.Tasks;
 
 namespace GenericAssignment_batch222
 {
-    internal class MaximumNumberCheck
+     class MaximumNumberCheck<T> where T : IComparable
     {
-        public static string MaximumString(string firstValue, string secondValue, string thirdValue)
+        public static void FindMax(T firstValue, T secondValue, T thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
                 firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
                 firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
             {
-                return firstValue;
+                Console.WriteLine($"{firstValue}  is greater in {firstValue}, {secondValue},{thirdValue} ");
             }
             else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
                 secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
                 secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
             {
-                return secondValue;
+                Console.WriteLine($"{secondValue}  is greater in {firstValue}, {secondValue},{thirdValue} ");
             }
             else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
                 thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
                 thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
             {
-                return thirdValue;
+                Console.WriteLine($"{thirdValue}  is greater in {firstValue}, {secondValue},{thirdValue} ");
             }
             else
-                return firstValue;
-        }
-        public static void Main()
-        {
-            Console.WriteLine("Welcome to Generic's Maximum Number Check");
-            Console.WriteLine("Enter three string's and find the max string");
-            Console.WriteLine("Enter first String");
-            string x = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Enter second String");
-            string y = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Enter third String");
-            string z = Convert.ToString(Console.ReadLine());
-            string output = MaximumNumberCheck.MaximumString(x, y, z);
-            Console.WriteLine(output + " is Maximum string");
-            Console.ReadKey();
-        }
+                Console.WriteLine($"{firstValue}  is greater ");
+        }      
 
     }
 }
---
+++ GenericAssginment-batch222/Program.cs
@@ -0,0 +1,21 @@
+﻿using GenericAssignment_batch222;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssginment_batch222
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Generic's Maximum Number Check");
+            MaximumNumberCheck<int>.FindMax(20, 30, 40);
+            MaximumNumberCheck<double>.FindMax(50.99, 30.36, 70.99);
+            MaximumNumberCheck<string>.FindMax("Apple", "Peach", "Banana");
+            Console.ReadKey();
+        }
+    }
+}
