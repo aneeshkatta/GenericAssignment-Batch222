7ec0bcd73ed83f09802c304bcca977b5d864302c
Aneesh <katta.aneesh597@gmail.com>
2022-11-25 14:02:00 +0530

    added welcome message

---
+++ GenericAssginment-batch222/MaximumNumberCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment_batch222
+{
+    internal class MaximumNumberCheck
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Generic's Maximum Number Check");
+           
+        }
+
+    }
+}
